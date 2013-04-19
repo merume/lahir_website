1dee1d175b011519126a7ab0687c8756bcebfac5
Masaki Sato <merume@gmail.com>
2013-04-19 15:08:10 +0900

    トップページ ロゴ中央表示 及び インフォメーション 表示位置 Fixed

--- _src/_js/index.html.js
+++ _src/_js/index.html.js
@@ -1,9 +1,27 @@
 $(function(){
-  var left = Math.floor(($(window).width() - $("#logo").width()) / 2);
-  var top  = Math.floor(($(window).height() - $("#logo").height()) / 2);
+  var element_middle = function(id_name){
+    var left = Math.floor(($(window).width() - $("#" + id_name).width()) / 2);
+    var top  = Math.floor(($(window).height() - $("#" + id_name).height()) / 2);
+    $("#" + id_name).css({
+      "position": "absolute",
+      "top": top,
+      "left": left,
+    });
+  };
 
-  $("#logo").css({
-    "top": top,
-    "left": left,
+  var info_fixed = function(){
+    var right = Math.floor(($(window).width() - $("#info-wrap").parent().width()) / 2)
+    $("#info-wrap").css({"right": right});
+  };
+
+  $(window).resize(function(){
+    element_middle("img-main");
+    info_fixed();
   });
+
+  $(document).ready(function(){
+    element_middle("img-main");
+    info_fixed();
+  });
+
 });
